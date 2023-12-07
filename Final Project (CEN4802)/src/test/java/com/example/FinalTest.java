1f7ca5d7358047132ecfaefa7f95a1d17be8a296
Siamak Erami <123394155+SiamakErami@users.noreply.github.com>
2023-12-07 17:40:08 -0500

    Add files via upload

---
+++ Final Project (CEN4802)/src/test/java/com/example/FinalTest.java
@@ -0,0 +1,31 @@
+package test.java.com.example;
+
+import org.junit.Test;
+
+import main.java.com.example.backgroundCode;
+
+public class FinalTest {
+
+    @Test(timeout = 5000)
+    public void testBackgroundCode() {
+        backgroundCode backgroundCode = new backgroundCode();
+
+        // Run the code in a separate thread
+        Thread thread = new Thread(() -> {
+            backgroundCode.main(null);
+        });
+
+        // Start the thread
+        thread.start();
+
+        // Let the thread run for a while (e.g., 5 seconds)
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Interrupt the thread to stop the infinite loop
+        thread.interrupt();
+    }
+} 
